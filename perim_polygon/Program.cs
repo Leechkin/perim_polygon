004e5311db1b54f830a8435d55bbeb5f7e0c78f4
fadeev <fadeev@DESKTOP-C4UMO1B>
2021-09-19 22:50:52 +0300

    Добавьте файлы проекта.

---
+++ perim_polygon/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace perim_polygon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double p, r;
+            int n;
+           
+            Console.WriteLine("Введите радиус окружности");
+            r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите количество сторон");
+            n = Convert.ToInt32(Console.ReadLine());
+            double al = 180 * Math.PI / 180;
+            p = n * 2 * r * Math.Sin(al / n);
+            Console.WriteLine($"Периметр равен {p}");
+        }
+    }
+}
